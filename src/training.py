33740e4a37648ddeb1484f26548b747455abb281
Liji Alex <liji.alex@gmail.com>
2021-10-09 06:57:30 +0530

    create model and train model

--- src/training.py
+++ src/training.py
@@ -1,11 +1,22 @@
 from utils.common import read_config
 from utils.data_mgmt import get_data
+from utils.model import create_model
 import argparse
 
 def training(config_path):
     config = read_config(config_path)
+
     validation_datasize = config["params"]["validation_datasize"]
     (X_train, y_train), (X_val, y_val), (X_test, y_test) = get_data(validation_datasize)
+
+    LOSS_FUNCTION = config["params"]["loss_functions"]
+    OPTIMIZER = config["params"]["optimizer"]
+    METRICS = config["params"]["metrics"]
+    model = create_model(LOSS_FUNCTION, OPTIMIZER, METRICS)
+
+    EPOCHS = config["params"]["epochs"]
+    VALIDATION = (X_val, y_val)
+    history = model.fit(X_train,y_train, epochs=EPOCHS, validation_data=VALIDATION)
 
 if __name__ == '__main__':
     args = argparse.ArgumentParser()
