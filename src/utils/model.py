fb14dc8c5dcb34ae3e8ce4d546570d6f85dcd3ea
Liji Alex <liji.alex@gmail.com>
2021-10-14 07:02:03 +0530

    added a save model function

--- src/utils/model.py
+++ src/utils/model.py
@@ -1,4 +1,6 @@
-import tensorflow as tf 
+import tensorflow as tf
+import time
+import os.path
 
 
 def create_model(LOSS_FUNCTION, OPTIMIZER, METRICS):
@@ -15,3 +17,12 @@ def create_model(LOSS_FUNCTION, OPTIMIZER, METRICS):
 
     model_clf.compile(loss=LOSS_FUNCTION, optimizer=OPTIMIZER, metrics=METRICS)
     return model_clf ## untrained model
+
+def get_unique_filename(filename):
+    unique_filename = time.strftime(f"%Y-%m-%d_%H%S%M_{filename}")
+    return unique_filename
+
+def save_model(model, model_name, model_dir):
+    unique_filename = get_unique_filename(model_name)
+    path_to_model = os.path.join(model_dir, unique_filename)
+    model.save(path_to_model)
